2073e824e9aa1562194caf3f0c7b1efa08d8d4bf
PoorUniversityStudent <94425486+ricefarmerjohn@users.noreply.github.com>
2023-10-05 02:10:36 -0400

    Initial commit

---
+++ Assignment 1/Lab1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n1,n2,n3,n4, small1, small2, large1, large2, largest, smallest;
+    printf("Enter four integers:  ");
+    scanf(" %d %d %d %d", &n1,&n2,&n3,&n4);
+
+    // This is to test out which of these little sharts are the smaller one
+    if(n1 < n2){
+        small1 = n1;
+        large1 = n2;
+    } else {
+        // Simple else statement in case n1 is greater than n2
+        large1 = n1;
+        small1 = n2;
+    }
+    // Same thing over here, we're just seeing which one is the smaller and larger one
+    if(n3 < n4){
+        small2 = n3;
+        large2 = n4;
+    } else {
+        large2 = n3;
+        small2 = n4;
+    }
+    // Now we compare which one is the smallest
+    if(small1 < small2){
+        smallest = small1;
+    } else {
+        smallest = small2;
+    }
+    // We do the same over here to see which one is the largest one
+    if(large1 < large2){
+        largest = large2;
+    } else {
+        largest = large1;
+    }
+    // We use \n to make the line count a bit less
+    printf("Largest: %d \n Smallest: %d", largest, smallest);
+}
---
+++ Assignment 1/Lab2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n, i;
+    printf("Enter number of days in month: ");
+    scanf("%d", &n);
+    printf("Enter starting day of the week (1 = Sun, 7 = Sat): ");
+    scanf("%d", &i);
+
+    for(int x = 0; x < i; x++){
+        printf("   ");
+    }
+    for(int m = 1; m < n; i++, m++){
+        printf("%d ", m);
+        if(i % 7 == 0){
+            printf("\n");
+        }
+    }
+    return 0;
+}
