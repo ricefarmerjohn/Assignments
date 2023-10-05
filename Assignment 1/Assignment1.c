1bef77f5ab21d0511d3001cc28f5fd9f8c651127
PoorUniversityStudent <94425486+ricefarmerjohn@users.noreply.github.com>
2023-10-05 18:31:28 -0400

    Got the conversion results to work now. I forgot to change %d to %f previously

--- Assignment 1/Assignment1.c
+++ Assignment 1/Assignment1.c
@@ -5,11 +5,13 @@
  */
 int main() {
     int i, n = 0, k = 0;
+    float amount;
     char ch;
-    printf("Welcome to the converter, enter an integer listed below to enter the action associated with that integer\n");
-    printf("1 for conversion between Kilograms and Pounds\n2 for conversion between Hectares and Acres\n3 for conversion between Litres and Gallons\n");
-    printf("4 for conversion between Kilometre and Mile\n5 to quit\n");
+
     while(n != 1) {
+        printf("Enter an integer listed below to enter the action associated with that integer\n");
+        printf("1 for conversion between Kilograms and Pounds\n2 for conversion between Hectares and Acres\n3 for conversion between Litres and Gallons\n");
+        printf("4 for conversion between Kilometre and Mile\n5 to quit\n");
         printf("Enter the integer here: ");
         scanf("%d", &i);
 
@@ -18,19 +20,32 @@ int main() {
                 printf("K for conversion from Kilograms to Pounds\nP for conversion from Pounds to Kilograms\n");
                 printf("Enter the character here: ");
                 scanf(" %c", &ch);
-
+                if(ch == 'K' || ch == 'k'){
+                    printf("Enter the amount here: ");
+                    scanf("%f", &amount);
+                    float conversion = amount * 2.20462;
+                    printf("Your conversion is: %f\n", conversion);
+                } else if(ch == 'P' || ch == 'p'){
+                    printf("Enter the amount here: ");
+                    scanf("%f", &amount);
+                    float conversion = amount / 2.20462;
+                    printf("Your conversion is: %f\n", conversion);
+                }
                 break;
             case 2:
-                printf("2 works\n");
+                printf("H for conversion from Hectares to Acres\nA for conversion from Acres to Hectares\n");
+                printf("Enter the character here: ");
+                scanf(" %c", &ch);
                 break;
             case 3:
-                printf("3 works\n");
+                printf("L for conversion from Litres to Gallons\nG for conversion from Gallons to Litres\n");
+                printf("Enter the character here: ");
+                scanf(" %c", &ch);
                 break;
             case 4:
-                printf("4 works\n");
-                break;
-            case 5:
-                printf("5 works\n");
+                printf("K for conversion from Kilometre to Mile\nM for conversion from Mile to Kilometre\n");
+                printf("Enter the character here: ");
+                scanf(" %c", &ch);
                 break;
             default:
                 break;
