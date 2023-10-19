477329bbb8bbb1e0c4a1ef8a17c7ab490263c3be
PoorUniStudent <94425486+ricefarmerjohn@users.noreply.github.com>
2023-10-19 14:40:29 -0400

    Fix things
    fixing stuff

--- Assignment 3/Part1.c
+++ Assignment 3/Part1.c
@@ -3,6 +3,8 @@
 int main() {
     int num;
 
+
+    // Get user input, then put input into array to define the length
     printf("Please enter the number of integers to process: ");
     scanf("%d", &num);
 }
