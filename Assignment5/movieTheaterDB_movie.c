134c8d6c472e3de2492e6e1cac509edf52e8221d
PoorUniversityStudent <94425486+ricefarmerjohn@users.noreply.github.com>
2023-11-29 23:10:54 -0500

    Me no understand

--- Assignment5/movieTheaterDB_movie.c
+++ Assignment5/movieTheaterDB_movie.c
@@ -1,29 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include "movieTheaterDB.h"
 #include "movieTheaterDB_movie.h"
 
-void insert(){
+void insert(struct movie *list, int n){
+    float temp;
+    struct movie *cur, *prev, *newNode;
+    newNode = malloc(sizeof(struct movie));
+    if (newNode == NULL) {
+        printf("Database is full; can't add more parts.\n");
+        return;
+    }
+    printf("Enter movie number: ");
+    scanf("%d", &newNode->movieCode);
+
+    for (cur = list, prev = NULL; cur != NULL && newNode->movieCode > cur->movieCode; prev = cur, cur = cur->next);
+        if (cur != NULL && newNode->movieCode == cur->movieCode) {
+            printf("Code already exists.\n");
+            free(newNode);
+            return;
+    }
+
+    printf("Enter movie name: ");
+    scanf(" %s", newNode->movieName);
+
+    printf("Enter movie genre: ");
+    scanf(" %s", newNode->movieGenre);
+
+    printf("Enter movie rating [0.0 - 10.0]: ");
+    scanf("%f", &temp);
+    if (temp < 0.0 || temp > 10.0) {
+        // If rating is not within the valid range, return without adding the movie
+        return;
+    } else {
+        newNode->movieRating = temp;
+    }
+
+    newNode->next = cur;
+    if (prev == NULL) {
+        list = newNode;
+    }else {
+        prev->next = newNode;
+    }
+}
+
+struct movie *search_list(struct movie *list, int code){
+    struct movie *p;
+    for (p = list; p != NULL; p = p->next)
+        if (p->movieCode == code)
+            return p;
+    return NULL;
+}
+
+// 99% sure this is done
+void print(struct movie *list) {
+    struct movie *p;
+    printf("Movie Code   Movie Name                 Movie Genre               Movie Rating\n");
+    for (p = list; p != NULL; p = p->next) {
+        printf("%-12d %-26s %-25s %.1f\n", p->movieCode, p->movieName, p->movieGenre, p->movieRating);
+    }
+}
+
+void update(struct movie *list, int code){
 
 }
 
-void search(){
-
-}
-
-void update(){
-
-}
-
-void print(){
-
-}
-
-void erase(){
-
+struct movie *delete(struct movie *list, int code){
+    struct movie *cur, *prev;
+    for (cur = list, prev = NULL;
+         cur != NULL && cur->movieCode != code;
+         prev = cur, cur = cur->next)
+        ;
+    if (cur == NULL)
+        return list; /* n was not found */
+    if (prev == NULL)
+        list = list->next; /* n is in the first node */
+    else
+        prev->next = cur->next; /* n is in some other node */
+    free(cur);
+    return list;
 }
 
 struct movie *movieMenu(struct movie *list){
+    int count = 0;
     char input;
 
     while (input != 'q') {
@@ -45,16 +106,18 @@ struct movie *movieMenu(struct movie *list){
                 printf("Exiting the movie database.\n");
                 break;
             case 'i':
-                printf("Sorry idk what I'm doing");
+                insert(list,count);
                 break;
             case 'p':
-                printf("Sorry idk what I'm doing 2");
+                print(list);
                 break;
             case 'u':
                 printf("Sorry idk what I'm doing 3");
                 break;
             case 's':
                 printf("Sorry idk what I'm doing 4");
+                break;
+            case 'e':
                 break;
             default:
                 printf("Invalid operation code, please try again\n");
--- Assignment5/movieTheaterDB_movie.h
+++ Assignment5/movieTheaterDB_movie.h
@@ -2,10 +2,10 @@
 #define ASSIGNMENT5_MOVIETHEATERDB_MOVIE_H
 
 struct movie *movieMenu(struct movie *list);
-void insert();
-void search();
-void update();
-void print();
-void erase();
+struct movie *search(struct movie *list, int code);
+void insert(struct movie *list, int code);
+void update(struct movie *list, int code);
+void print(struct movie *list);
+struct movie *delete(struct movie *list, int code);
 
 #endif //ASSIGNMENT5_MOVIETHEATERDB_MOVIE_H
